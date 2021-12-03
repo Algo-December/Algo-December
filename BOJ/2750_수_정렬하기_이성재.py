6b4e270505f3b7dc90cb3cea0541421f4cc23eef
seongjaee <labyrinthird@gmail.com>
2021-12-03 18:14:20 +0900

    2750

---
+++ BOJ/2750_수_정렬하기_이성재.py
@@ -0,0 +1,21 @@
+import sys
+input = sys.stdin.readline
+
+pos_nums = [False] * 1001  # 0 ~ 1000
+neg_nums = [False] * 1001  # -1 ~ -1000
+
+n = int(input())
+for _ in range(n):
+    a = int(input())
+    if a >= 0:
+        pos_nums[a] = True
+    else:
+        neg_nums[-a] = True
+
+for i in range(1000, -1, -1):
+    if neg_nums[i]:
+        print(-i)
+
+for i in range(1001):
+    if pos_nums[i]:
+        print(i)
