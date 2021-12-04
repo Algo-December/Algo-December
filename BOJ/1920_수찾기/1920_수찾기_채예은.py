bb13a39fdd51ba1ec0896b3fcf351066ed07c713
Yen_tree <codpdms@naver.com>
2021-12-04 11:41:10 +0900

    Edit: 1920 2751 & Solve: 10989

--- BOJ/1920_수찾기/1920_수찾기_채예은.py
+++ BOJ/1920_수찾기/1920_수찾기_채예은.py
@@ -1,20 +1,13 @@
-# 이진 탐색으로 풀었다.
-def binary_search(arr, value, low, high):
-    if low > high:
-        return False  # 탐색 실패
-    mid = (low + high) // 2
-    if arr[mid] > value:
-        return binary_search(arr, value, low, mid-1)
-    elif arr[mid] < value:
-        return binary_search(arr, value, mid+1, high)
-    else:
-        return arr[mid]
-
+import sys
+input = sys.stdin.readline
 
 N = int(input())
-nums1 = sorted(list(map(int, input().split())))
+A = set(map(int, input().split()))
 M = int(input())
-nums2 = list(map(int, input().split()))
+finds = list(map(int, input().split()))
 
-for i in range(M):
-    print(1 if binary_search(nums1, nums2[i], 0, N-1) else 0)
+for find in finds:
+    if find in A:
+        print(1)
+    else:
+        print(0)
