ae5bcbca656ca1d57053d0172691981c42506b68
seongjaee <labyrinthird@gmail.com>
2021-12-17 23:33:42 +0900

    Edit: 15686

--- BOJ/15686_치킨배달/15686_치킨배달_이성재.py
+++ BOJ/15686_치킨배달/15686_치킨배달_이성재.py
@@ -5,6 +5,7 @@ input = sys.stdin.readline
 n, m = map(int, input().split())
 graph = [input().split() for _ in range(n)]
 
+# 집과 치킨집의 좌표 저장
 houses = []
 chickens = []
 for i in range(n):
@@ -14,6 +15,8 @@ for i in range(n):
         elif graph[i][j] == '1':
             houses.append((i, j))
 
+# 각 집과 치킨집 사이의 거리를 2차원 배열로 저장
+# distances[i][j] : i번째 가정집과 j번째 치킨집 사이 거리
 distances = []
 for hy, hx in houses:
     distances.append([])
@@ -24,8 +27,8 @@ for hy, hx in houses:
 result = 1e10
 for comb in combinations(range(len(chickens)), m):
     now = 0
-    for d in distances:
-        now += min([d[i] for i in comb])
+    for d in distances:  # d: 한 가정집의 치킨집들까지의 거리 배열
+        now += min([d[i] for i in comb])  # 고른 m개의 치킨집까지의 거리들 중 최솟값
 
     result = min(result, now)
     if result == m:
