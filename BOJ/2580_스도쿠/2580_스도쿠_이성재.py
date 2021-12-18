a0f4d6b1ef6b84741566a1e29c9258285e2b3f70
seongjaee <labyrinthird@gmail.com>
2021-12-18 12:46:02 +0900

    Edit: 2580

--- BOJ/2580_스도쿠/2580_스도쿠_이성재.py
+++ BOJ/2580_스도쿠/2580_스도쿠_이성재.py
@@ -1,39 +1,39 @@
 import sys
 input = sys.stdin.readline
 
-
-def get_candidates(y, x):
-    # 빈 칸 (y, x)에 들어갈 수 있는 숫자 후보들 구하는 함수
-    result = []
-    row = graph[y]
-    col = [graph[i][x] for i in range(9)]
-
-    square = set()
-    ly, lx = (y // 3) * 3, (x // 3) * 3  # (ly, lx): (y, x)가 포함된 작은 3x3 정사각형의 좌상단 좌표 
-    for i in range(ly, ly + 3):
-        for j in range(lx, lx + 3):
-            square.add(graph[i][j])
-
-    for i in range(1, 10):
-        if i in row or i in col or i in square:
-            continue
-        result.append(i)
-    
-    return result
-
-
 def backtrack(level):
     if level == m:
         for row in graph:
             print(' '.join(map(str, row)))
         sys.exit()
-        
-    now_y, now_x = blanks[level]
-    for candidate in get_candidates(now_y, now_x):
-        graph[now_y][now_x] = candidate
-        backtrack(level + 1)
-        graph[now_y][now_x] = 0
 
+    now_y, now_x = blanks[level]
+    for num in range(1, 10):
+        if not row_cndts[now_y][num]:
+            continue
+        if not col_cndts[now_x][num]:
+            continue
+        if not square_cndts[now_y // 3][now_x // 3][num]:
+            continue
+        
+        # 방문 처리
+        graph[now_y][now_x] = num
+        row_cndts[now_y][num] = False
+        col_cndts[now_x][num] = False
+        square_cndts[now_y // 3][now_x // 3][num] = False
+
+        backtrack(level + 1)
+        
+        # 원래대로
+        graph[now_y][now_x] = 0
+        row_cndts[now_y][num] = True
+        col_cndts[now_x][num] = True
+        square_cndts[now_y // 3][now_x // 3][num] = True
+
+
+row_cndts = [[True] * 10 for _ in range(9)]  # row_cndts[i][num]: i행에 num이 올 수 있나요??
+col_cndts = [[True] * 10 for _ in range(9)]  # col_cndts[j][num]: j열에 num이 올 수 있나요??
+square_cndts = [[[True] * 10 for _ in range(3)] for _ in range(3)]  # square_cndts[i][j][num]: (i, j) 정사각형에 num이 올 수 있나요??
 
 graph = [list(map(int, input().split())) for _ in range(9)]
 
@@ -41,8 +41,13 @@ graph = [list(map(int, input().split())) for _ in range(9)]
 blanks = []
 for i in range(9):
     for j in range(9):
-        if graph[i][j] == 0:
+        num = graph[i][j]
+        if num == 0:
             blanks.append((i, j))
+        else:
+            row_cndts[i][num] = False
+            col_cndts[j][num] = False
+            square_cndts[i // 3][j // 3][num] = False
 
 m = len(blanks)
 backtrack(0)
