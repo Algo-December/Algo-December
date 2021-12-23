639c077994a19fc8522fb35acfff1e11a5de3cf3
Yen_tree <codpdms@naver.com>
2021-12-23 16:53:41 +0900

    Solve: 2178

--- BOJ/2178_미로탐색/2178_미로탐색_채예은.py
+++ BOJ/2178_미로탐색/2178_미로탐색_채예은.py
@@ -1,32 +1,31 @@
 import sys
+from collections import deque
 input = sys.stdin.readline
 
+
+def bfs():
+    q = deque()
+    q.append((0, 0, 1))  # 시작점(0, 0) q에 추가
+    check[0][0] = 1
+
+    while q:
+        x, y, cnt = q.popleft()
+
+        for dr, dc in DIRECTION:
+            nr, nc = x + dr, y + dc
+
+            if 0 <= nr < n and 0 <= nc < m and matrix[nr][nc] == '1' and not check[nr][nc]:
+                check[nr][nc] = 1
+                q.append((nr, nc, cnt+1))
+
+                if nr == n-1 and nc == m-1:  # 도착점 (n-1, m-1)
+                    print(cnt+1)
+                    return
+
+
+n, m = map(int, input().split())
+matrix = [input() for _ in range(n)]
+check = [[0]*m for _ in range(n)]
 DIRECTION = ((0, 1), (1, 0), (0, -1), (-1, 0))
 
-
-def dfs(r, c):
-    global cnt
-    cnt += 1
-    check[r][c] = 1
-    for dr, dc in DIRECTION:
-        nr, nc = r + dr, c + dc
-        if 0 <= nr < n and 0 <= nc < n and matrix[nr][nc] == '1' and not check[nr][nc]:
-            dfs(nr, nc)
-
-
-n = int(input())
-matrix = [input() for _ in range(n)]
-check = [[0]*n for _ in range(n)]
-
-towns = []
-for r in range(n):
-    for c in range(n):
-        if matrix[r][c] == '1' and not check[r][c]:
-            # 단지탐색
-            cnt = 0  # 현재 단지 집의 수
-            dfs(r, c)
-            towns.append(cnt)
-
-print(len(towns))
-for town in sorted(towns):  # 오름차순 정렬
-    print(town)
+bfs()
