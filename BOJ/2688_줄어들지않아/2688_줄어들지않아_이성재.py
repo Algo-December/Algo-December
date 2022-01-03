cb939417cd5b07a50361da0f322bbc9c84278dc3
seongjaee <labyrinthird@gmail.com>
2022-01-03 16:16:10 +0900

    Comment: 2688

--- BOJ/2688_줄어들지않아/2688_줄어들지않아_이성재.py
+++ BOJ/2688_줄어들지않아/2688_줄어들지않아_이성재.py
@@ -1,9 +1,9 @@
 import sys
 input = sys.stdin.readline
 
-# meme: {key: value}
+# memo: {key: value}
 # value[num] : num으로 끝나는 줄어들지 않는 key자리 수의 개수
-memo = {1: [1] * 10,}  
+memo = {1: [1] * 10,}
 
 for i in range(2, 65):
     pre = memo[i - 1][:]  # 누적 합
@@ -15,3 +15,13 @@ T = int(input())
 for _ in range(T):
     n = int(input())
     print(sum(memo[n]))
+
+
+"""
+    0  1  2  3  4  5  6  7  8  9
+1: [1, 1, 1, 1, 1, 1, 1, 1, 1, 1]  => 10
+2: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10] => 55
+3: [1, 3, 6, 10, 15, 21, 28, 36, 45, 55] => 220
+4: [1, 4, 10, 20, 35, 56, 84, 120, 165, 220] => 715
+
+"""
