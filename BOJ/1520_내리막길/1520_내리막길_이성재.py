7a7bbfdc6c6b45e0e90ec415f420c33eb4f5fe38
이성재 <xpsxm468@naver.com>
2022-03-05 11:34:59 +0900

    Edit: 1520

--- BOJ/1520_내리막길/1520_내리막길_이성재.py
+++ BOJ/1520_내리막길/1520_내리막길_이성재.py
@@ -10,16 +10,13 @@ dp_matrix[0][0] = 1
 
 def dp(i, j):
     if 0 <= i < m and 0 <= j < n:
-        if dp_matrix[i][j] != -1:
+        if dp_matrix[i][j] != -1:  # 이미 계산된 적 있으면 바로 반환
             return dp_matrix[i][j]
 
-        dp_matrix[i][j] = 0
+        dp_matrix[i][j] = 0  # 방문 처리
         for ni, nj in [(i - 1, j), (i + 1, j), (i, j + 1), (i, j - 1)]:
             if 0 <= ni < m and 0 <= nj < n and graph[ni][nj] > graph[i][j]:
-                if dp_matrix[ni][nj] == -1:
-                    dp_matrix[i][j] += dp(ni, nj)
-                else:
-                    dp_matrix[i][j] += dp_matrix[ni][nj]
+                dp_matrix[i][j] += dp(ni, nj)
 
     return dp_matrix[i][j]
 
